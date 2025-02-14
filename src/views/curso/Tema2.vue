45fe6c075787ef52b5fa7c87cea7aa8df85aa07a
Francisco Lizcano <flizcanor@icloud.com>
2025-02-13 23:06:39 -0500

    Avance tema 2

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -1,10 +1,38 @@
 <template lang="pug">
   .curso-main-container.pb-3
-    BannerInterno(:subTitulo="'2. Título tema'")
+    BannerInterno(:subTitulo="'2. Gestión del talento en la vida cotidiana y en las organizaciones'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
   
-      #t_2_1.titulo-segundo(data-aos="flip-up")
-        h2 #[span 2.1] Subtítulo tema
+
+      .row.mb-5.align-items-center
+        .col-lg-8
+
+          .bg-color-1.p-4(data-aos="fade-left").mb-4
+            p.mb-0 Después de comprender la importancia y transformación del talento humano, es esencial analizar los subprocesos que lo integran y su impacto en el desarrollo organizacional.
+
+          h3(data-aos="fade-right").mb-4 Procesos que pueden o no ser parte de recursos humanos
+          
+          p(data-aos="fade-right") Algunos subprocesos pueden estar dentro del departamento de recursos humanos o ser gestionados de manera independiente, dependiendo de la estrategia organizacional. Estos procesos incluyen:
+
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/1.png", data-aos="zoom-in")
+
+      .row.align-items-start.mb-5
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/2.png", alt="").mb-4.mb-lg-0
+        .col-lg-8
+
+          AcordionA(tipo="b")
+            .div(titulo="Gestión de nómina")
+              p Incluye la generación de contratos y afiliaciones a seguridad social, el manejo de la documentación laboral, la gestión de pagos fijos, variables y beneficios extralegales, así como la elaboración de reportes contables y el cumplimiento normativo.
+
+            .div(titulo="Gestión legal y laboral")
+              p Abarca la elaboración de contratos y documentos legales, la implementación de políticas laborales, la ejecución de procedimientos disciplinarios, como llamados de atención y suspensiones, y el equilibrio entre el cumplimiento normativo y el bienestar del trabajador.
+
+            .div(titulo="Seguridad y salud en el trabajo")
+              p Comprende la evaluación de riesgos y el desarrollo de políticas preventivas, la implementación de capacitaciones en seguridad, la reducción de accidentes laborales y la mejora del clima organizacional.
 
       .bg-full-width.border-top.color-primario
         .p-4.p-md-5
